9b507976aeb4bc3c79bc7095399dbe337a6fa05e
wanglanrixitroy <37648888+wanglanrixitroy@users.noreply.github.com>
2023-03-16 16:31:56 +0000

    adding the tests

--- hello.py
+++ hello.py
@@ -1,2 +1,28 @@
-def hello():
-    return "hi"
+#!/usr/bin/env python
+import click
+import glob
+
+
+@click.command()
+@click.option(
+    "--path",
+    prompt="Path to search for csv files",
+    help="This is the path to search for files: /tmp",
+)
+@click.option(
+    "--ftype", prompt="Pass in the type of file", help="Pass in the file type: i.e csv"
+)
+def search(path, ftype):
+    """
+    This is a tool that search for patterns like *.csv
+    """
+
+    results = glob.glob(f"{path}/*.{ftype}")
+    click.echo(click.style("Found Matches:", fg="red"))
+    for result in results:
+        click.echo(click.style(f"{result}", bg="green", fg="white"))
+
+
+if __name__ == "__main__":
+    # pylint: disable*no-value-for-parameter
+    search()
