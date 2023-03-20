36524cd708ab182aedfb78293b1627aa997cedff
EC2 Default User <ec2-user@ip-172-31-27-225.ec2.internal>
2023-03-20 14:29:24 +0000

    add cv test

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import click
 
+
 @click.command()
 @click.option("--name")
 def hello(name):
     click.echo(f"Hello {name}!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
-    hello() 
+    hello()
